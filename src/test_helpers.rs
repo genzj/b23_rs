57e2f6bf0f2176f5fd4755113218f3092e80f07b
genzj <zj0512@gmail.com>
2026-07-09 01:57:08 -0700

    chore(tests): update mocked redirect URL to match actual response

--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -8,7 +8,7 @@ pub async fn setup_mock_server() -> MockServer {
 
     // Mock /rlUSCcz to redirect to the video page on the same mock server
     let redirect_url = format!(
-        "{}/video/BV1BECcB3EG6?p=1&spm_id_from=333.999.0.0&vd_source=12345",
+        "{}/video/BV1BECcB3EG6?buvid=XUEB1C5EA22C98FB0BF8D33A1BA11D12A2B2D&from_spmid=tm.recommend.0.0&is_story_h5=false&mid=9mRlviQUEP0n%2BrijVaCSQH8FTQ%2FSZMtL1rElX6M3iMo%3D&p=1&plat_id=116&share_from=ugc&share_medium=android_i&share_plat=android&share_session_id=c14d6a0b-152c-453f-9b2e-f776bab6869c&share_source=COPY&share_tag=s_i&spmid=united.player-video-detail.0.0&timestamp=1762786023&unique_k=rlUSCcz&up_id=3493279559584443",
         mock_server.uri()
     );
     Mock::given(method("GET"))
